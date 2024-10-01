c7915bf62309b7339585d7ff644014db78ec614d
Jared Peter <aljapeter05@gmail.com>
2024-09-30 22:05:02 -0300

    added prompts for development

---
+++ prompts.py
@@ -0,0 +1,18 @@
+x1 = (int)(input("Ingrese el coefieciente de x: "))
+y1 = (int)(input("Ingrese el coefieciente de y: "))
+z1 = (int)(input("Ingrese el coefieciente de z: "))
+respuesta1 = (int)(input("Toda la ecuacion es igual a: "))
+
+print("Segunda ecuacion: ")
+
+x2 = (int)(input("Ingrese el coefieciente de x: "))
+y2 = (int)(input("Ingrese el coefieciente de y: "))
+z2 = (int)(input("Ingrese el coefieciente de z: "))
+respuesta2 = (int)(input("Toda la ecuacion es igual a: "))
+
+print("Tercera ecuacion: ")
+
+x3 = (int)(input("Ingrese el coefieciente de x: "))
+y3 = (int)(input("Ingrese el coefieciente de y: "))
+z3 = (int)(input("Ingrese el coefieciente de z: "))
+respuesta3 = (int)(input("Toda la ecuacion es igual a: "))
