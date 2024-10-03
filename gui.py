ec1aa1ac8d6aa40afb37ba0eba8ff6dc81d3b051
Jared Peter <aljapeter05@gmail.com>
2024-10-03 12:04:35 -0300

    made gui

---
+++ gui.py
@@ -0,0 +1,118 @@
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QFont
+
+from main import convertir_fila_en_uno, restar_filas, calcular_segunda_fila, calcular_respuestas
+
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget, QHBoxLayout
+import sys
+
+variables = ['x +', 'y +', 'z', '=']
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.inputs = []
+
+        self.setWindowTitle("Calculadora Gauss")
+        self.resize(QSize(600, 400))
+        
+        main_layout = QVBoxLayout()
+        main_layout.setAlignment(Qt.AlignHCenter)
+        main_layout.setContentsMargins(0, 45, 0, 0)
+        main_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignCenter)
+
+        heading = QLabel("Gauss Calculator")
+        heading.setFont(QFont('Segoe UI', 24, QFont.Weight.Bold))
+
+        button = QPushButton("Calcular")
+        button.clicked.connect(self.calculate)
+
+        clear_button = QPushButton("Borrar")
+        clear_button.clicked.connect(self.clear)
+
+        self.message_heading = QLabel("Respuesta: ")
+        self.message_heading.setFont(QFont('Segoe UI', 14, QFont.Weight.Bold))
+        self.message_heading.setContentsMargins(0, 20, 0, 0)
+        self.message_heading.hide()
+
+        self.message = QLabel("")
+        self.message.setFont(QFont('Segoe UI', 10.5))
+        
+        container_element = QVBoxLayout()
+        for i in range(3):
+            row = self.create_matrix_row()
+            container_element.addLayout(row)
+
+        container = QWidget()
+        container.setLayout(container_element)
+        container.setMaximumWidth(300)
+
+        main_layout.addWidget(heading)
+        main_layout.addWidget(container)
+        main_layout.addWidget(button)
+        main_layout.addWidget(clear_button)
+        main_layout.addWidget(self.message_heading)
+        main_layout.addWidget(self.message)
+
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
+
+    def create_matrix_row(self):
+        layout = QHBoxLayout()
+        for i in range(4):
+            input = QLineEdit()
+            input.setMaximumWidth(30)
+            label = QLabel(variables[i])
+
+            if variables[i] == '=':
+                layout.addWidget(label)
+                layout.addWidget(input)
+            else:
+                layout.addWidget(input)
+                layout.addWidget(label)
+            self.inputs.append(input)
+        return layout
+    
+    def clear(self):
+        for number in self.inputs:
+            number.setText('')
+        self.message_heading.hide()
+        self.message.hide()
+    
+    def print_matrix(self, matrix):
+        for fila in matrix:
+            print(fila)
+        print('\n\n')
+
+    def calculate(self):
+        values = [input.text() for input in self.inputs]
+
+        matrix_numbers = []
+        try:
+            matrix_numbers = [float(value) for value in values]
+        except ValueError:
+            self.message.setText("Todos los valores tienen que ser números")
+            return 
+        
+        matrix = [matrix_numbers[i:i + 4] for i in range(0, len(matrix_numbers), 4)]
+        matrix[0] = convertir_fila_en_uno(matrix)
+        matrix[1] = restar_filas(matrix, 1)
+        matrix[2] = restar_filas(matrix, 2)
+        self.print_matrix(matrix)
+        matrix[1], matrix[2] = calcular_segunda_fila(matrix)
+        self.print_matrix(matrix)
+        matrix[2] = restar_filas(matrix, 2, 1)
+
+        self.message_heading.show()
+        self.message.show()
+        self.message.setText(calcular_respuestas(matrix))
+    
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
--- prompts.py
+++ prompts.py
@@ -16,125 +16,16 @@
 # y3 = (int)(input("Ingrese el coefieciente de y: "))
 # z3 = (int)(input("Ingrese el coefieciente de z: "))
 # respuesta3 = (int)(input("Toda la ecuacion es igual a: "))
-#
-#
+
+# matriz = [
+#     [x1, y1, z1, respuesta1],
+#     [x2, y2, z2, respuesta2],
+#     [x3, y3, z3, respuesta3]
+# ]
 
 
-
-
-
-
-
-
-
-print("Primera ecuacion: ")
-
-x1 = 3
-y1 = 4
-z1 = 2
-respuesta1 = 6
-
-print("Segunda ecuacion: ")
-
-x2 = 6
-y2 = 9
-z2 = 1
-respuesta2 = 7
-
-print("Tercera ecuacion: ")
-
-x3 = 3
-y3 = 6
-z3 = 2
-respuesta3 = 8
-
-matriz = [
-    [x1, y1, z1],
-    [x2, y2, z2],
-    [x3, y3, z3]
-]
-
-respuestas = [respuesta1, respuesta2, respuesta3]
-
-def cambiar_respuesta(respuesta, numero_para_mult, referencia_cambio):
-    resp = 0
-    if respuesta > 0:
-        resp = respuesta - (numero_para_mult*referencia_cambio)
-    else:
-        resp = respuesta + (numero_para_mult*referencia_cambio)
-    return resp
-
-def cambiar_fila_a_1(i, j, num):
-    return matriz[i][j]*(1/num)
-
-def check_if_matrix_done():
-    if matriz[1][0] == 0 and matriz[2][0] == 0 and matriz[2][1] == 0:
-        get_answers()
-
-def get_answers():
-    z = round(respuestas[2]/matriz[2][2], 1)
-    y = round((respuestas[1] - (matriz[1][2]*z))/matriz[1][1], 1)
-    x = round((respuestas[0] - (matriz[0][1]*y) - (matriz[0][2]*z))/matriz[0][0], 1)
-
-    print(x, y, z)
-
-# setear la primera fila
-for i in range(len(matriz[0])):
-    matriz[0][i] = cambiar_fila_a_1(0, i, x1)
-respuestas[0] = respuestas[0]*(1/x1)
-
-# segunda fila
-for i in range(len(matriz[1])):
-    if x2 > 0:
-        matriz[1][i] = matriz[1][i] - (x2*matriz[0][i])
-    else:
-        matriz[1][i] = matriz[1][i] + (x2*matriz[0][i])
-respuestas[1] = cambiar_respuesta(respuestas[1], x2, respuestas[0])
-
-# tercera fila
-for i in range(len(matriz[2])):
-    if x3 > 0:
-        matriz[2][i] = matriz[2][i] - (x3*matriz[0][i])
-    else:
-        matriz[2][i] = matriz[2][i] + (x3*matriz[0][i])
-respuestas[2] = cambiar_respuesta(respuestas[2], x3, respuestas[0])
-
-
-
-numero_medio = matriz[1][1]
-
-if numero_medio == 0:
-    fila_medio = matriz[1]
-    ultima_fila = matriz[2]
-
-    respuesta_medio = respuestas[1]
-    ultima_respuesta = respuestas[2]
-
-    matriz[1] = ultima_fila
-    matriz[2] = fila_medio
-    respuestas[1] = ultima_respuesta
-    respuestas[2] = respuesta_medio
-
-
-
-    print('\n\n')
-    numero_medio = matriz[1][1]
-
-check_if_matrix_done()
-# convertir el numero del medio en 1
-for i in range(len(matriz[1])):
-    matriz[1][i] = cambiar_fila_a_1(1, i, numero_medio)
-respuestas[1] = respuestas[1]*(1/numero_medio)
-
-numero_a_cambiar = matriz[2][1]
-for i in range(len(matriz[2])):
-    if numero_a_cambiar > 0:
-        matriz[2][i] = matriz[2][i] - (numero_a_cambiar*matriz[1][i])
-    else:
-        matriz[2][i] = matriz[2][i] + (numero_a_cambiar*matriz[1][i])
-respuestas[2] = cambiar_respuesta(respuestas[2], numero_a_cambiar, respuestas[1])
-
-for fila in matriz:
-    print(fila)
-
-get_answers()
+# matriz[0] = convertir_fila_en_uno(matriz)
+# matriz[1] = restar_filas(matriz, 1)
+# matriz[2] = restar_filas(matriz, 2)
+# matriz[1], matriz[2] = calcular_segunda_fila(matriz)
+# matriz[2] = restar_filas(matriz, 2, 1)
