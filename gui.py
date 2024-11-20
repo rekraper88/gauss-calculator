54740ddfcc3eb30a160102a6f4aaac0fb3023bfc
Jared Peter <aljapeter05@gmail.com>
2024-11-20 14:43:30 -0300

    comentarios

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import QFont
-from main import convertir_fila_en_uno, restar_filas, calcular_segunda_fila, calcular_respuestas
+from main import convertir_fila_en_uno, restar_filas, calcular_ultima_fila, calcular_respuestas
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget, QHBoxLayout
 import sys
 
@@ -8,16 +8,17 @@ variables = ['x +', 'y +', 'z', '=']
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # se inician los inputs
         self.inputs = []
-
+        # titulo
         self.setWindowTitle("Calculadora Gauss")
         self.resize(QSize(600, 400))
-
+        # coso principal
         main_layout = QVBoxLayout()
         main_layout.setAlignment(Qt.AlignHCenter)
         main_layout.setContentsMargins(0, 45, 0, 0)
         main_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignCenter)
-
+        # titulo
         heading = QLabel("Gauss Calculator")
         heading.setFont(QFont('Segoe UI', 24, QFont.Weight.Bold))
 
@@ -27,19 +28,20 @@ class MainWindow(QMainWindow):
         clear_button = QPushButton("Resetear")
         clear_button.clicked.connect(self.clear)
 
+        # Mensajes con la respuesta. Se esconde hasta que se haya calculado todo
         self.message_heading = QLabel("Respuesta: ")
         self.message_heading.setFont(QFont('Segoe UI', 14, QFont.Weight.Bold))
         self.message_heading.setContentsMargins(0, 20, 0, 0)
         self.message_heading.hide()
-
         self.message = QLabel("")
         self.message.setFont(QFont('Segoe UI', 10.5))
-
+        # elemento que contiene los inputs
         container_element = QVBoxLayout()
+        # en un rango de 3, crear tres filas, cada una de las cuales contiene 4 inputs
         for i in range(3):
             row = self.create_matrix_row()
             container_element.addLayout(row)
-
+        # contenedor
         container = QWidget()
         container.setLayout(container_element)
         container.setMaximumWidth(300)
@@ -58,10 +60,11 @@ class MainWindow(QMainWindow):
     def create_matrix_row(self):
         layout = QHBoxLayout()
         for i in range(4):
+            # se crean 4 inputs: x + y + z = respuesta
             input = QLineEdit()
             input.setMaximumWidth(30)
             label = QLabel(variables[i])
-
+            # de acuerdo a las variables posibles, agregarlas a la ecuacion
             if variables[i] == '=':
                 layout.addWidget(label)
                 layout.addWidget(input)
@@ -72,33 +75,31 @@ class MainWindow(QMainWindow):
         return layout
 
     def clear(self):
+        # borrar todo el contenido y sacar los mensajes de respuesta
         for number in self.inputs:
             number.setText('')
         self.message_heading.hide()
         self.message.hide()
 
-    def print_matrix(self, matrix):
-        for fila in matrix:
-            print(fila)
-        print('\n\n')
-
     def calculate(self):    
+        # se consigue los valores de todos los inputs y se los pone en un array
         values = [input.text() for input in self.inputs]
         matrix_numbers = []
         matrix = []
+        # si alguno de los inputs no es un numero, retornar error
         try:
             matrix_numbers = [float(value) for value in values]
         except ValueError:
             self.message.setText("Todos los valores tienen que ser números")
             return
-
+        # popular el array `matrix` con valores. El loop toma de a cuatro. O sea, de los 12 numeros va a tomar 0, 4, 8, 12
         for i in range(0, len(matrix_numbers), 4):
             matrix.append(matrix_numbers[i:i+4])
-            
+        # convertir la primera fila en 1
         matrix[0] = convertir_fila_en_uno(matrix)
         matrix[1] = restar_filas(matrix, 1)
         matrix[2] = restar_filas(matrix, 2)
-        matrix[1], matrix[2] = calcular_segunda_fila(matrix)
+        matrix[1], matrix[2] = calcular_ultima_fila(matrix)
         matrix[2] = restar_filas(matrix, 2, 1)
 
         self.message_heading.show()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def convertir_fila_en_uno(matriz, i=0):
-    print(matriz)
+    # si el primer elemento es 0
     if matriz[0][0] == 0:
+        # intercambiar las filas si no tiene un 0 en la primera posicion
         if matriz[1][0] != 0:
             # intercambiar la fila 1 con fila 2 y convertirlo de nuevo
             matriz[0], matriz[1] = matriz[1], matriz[0]
             return convertir_fila_en_uno(matriz)
+        # intercambiar las filas si no tiene un 0 en la primera posicion
         elif matriz[2][0] != 0:
             matriz[0], matriz[2] = matriz[2], matriz[0]
             return convertir_fila_en_uno(matriz)
@@ -13,41 +15,53 @@ def convertir_fila_en_uno(matriz, i=0):
 
     nueva_fila = []
     primer_elemento = matriz[i][i]
-
+    # agregar un elemento en el array multiplicado por el inverso del primer elemento
     for numero in matriz[i]:
         nueva_fila.append(numero*(1/primer_elemento))
     return nueva_fila
 
 def restar_filas(matriz, indice_fila, elem=0):
     nueva_fila = []
-    primer_elemento = matriz[indice_fila][elem]
-
-    if primer_elemento == 0:
+    elemento_a_convertir = matriz[indice_fila][elem]
+    
+    # si el numero a convertir es 0, entonces no es necesario hacer la resta
+    if elemento_a_convertir == 0:
         return matriz[indice_fila]
     else:
+        # por cada numero de la fila, agregar al array ese numero - el elemento a convertir multiplicado por el numero arriba
+        # F2: F2 - (F2*F1)
         for i, numero in enumerate(matriz[indice_fila]):
-            nueva_fila.append(numero - (primer_elemento*matriz[elem][i]))
+            nueva_fila.append(numero - (elemento_a_convertir*matriz[elem][i]))
         return nueva_fila
 
-def calcular_segunda_fila(matriz):
+def calcular_ultima_fila(matriz):
     nueva_fila = []
     elemento_medio = matriz[1][1]
 
+    # si el elemento del medio es 0:
     if elemento_medio == 0:
+        # si el elemento de abajo es 0, entonces no cambiar nada
         if matriz[2][1] == 0:
             return matriz[1], matriz[2]
+        # si es un numero no 0, intercambiar la segunda y ultima fila
         else:
             return matriz[2], matriz[1]
+    # si el elemento del medio no es 0:
     else:
-        nueva_fila = convertir_fila_en_uno(matriz, 1)
-        return nueva_fila, matriz[2]
+        # convertir la fila del medio en 0
+        segunda_fila = convertir_fila_en_uno(matriz, 1)
+        matriz[1] = segunda_fila
+        # restar la fila del medio y la segunda para obtener el ultimo elemento
+        ultima_fila = restar_filas(matriz, 2, 1)
+        return segunda_fila, ultima_fila
 
 
 def calcular_respuestas(matriz):
+    # si alguno de los elementos de la diagonal es 0 entonces no hay solucion unica
     for i in range(3):
         if matriz[i][i] == 0:
             return "La matriz no tiene solucion unica."
-
+    # calcular las incognitas en forma de ecuacion
     z = matriz[2][3]/matriz[2][2]
     y = (matriz[1][3]-(matriz[1][2]*z))/matriz[1][1]
     x = (matriz[0][3] - (matriz[0][2]*z) - (matriz[0][1]*y))/matriz[0][0]
